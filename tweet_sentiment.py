2478d19a8a6685904cde6a804ef9229c61c8f923
wendy <wendykan@gmail.com>
2014-07-10 08:46:21 -0700

    use the sentiment method outside

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,13 +1,6 @@
 import sys
 import json
-# loads the sentiment library
-def sentiment_lib(sent_file):
-  sent_lines = open(sent_file)
-  scores = {} # initialize an empty dictionary
-  for line in sent_lines:
-    term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
-    scores[term] = int(score)  # Convert the score to an integer.
-  return scores
+from sentiment import sentiment, sentiment_lib
   
 # reads the whole tweet log and parse it, calculates the sentiment of each tweet
 def score_each_tweet(tweet_file, sent_lib):
@@ -16,17 +9,8 @@ def score_each_tweet(tweet_file, sent_lib):
       tweet = json.loads(line , 'utf=8')
       if 'text' in tweet.keys():
         score = sentiment(tweet['text'], sent_lib)
-        print '<sentiment:',score,'>', tweet['text'] 
+        print score
            
-# calculates sentiment of the tweet input param
-def sentiment(tweet, sent_lib):
-  words = unicode(tweet).split()
-  score = 0.0
-  for word in words:
-    word_score = sent_lib.get(word, 0.0)
-    score += word_score
-  return score
-
 def main():
     sent_file = sys.argv[1]
     tweet_file = sys.argv[2]
